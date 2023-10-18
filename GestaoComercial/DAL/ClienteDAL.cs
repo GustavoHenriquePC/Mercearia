5abd1fc35e2e3f74ea7fd05092dd2108c0e08f7f
Gustavo Henrique <148218604+GustavoHenriquePC@users.noreply.github.com>
2023-10-18 11:27:41 -0300

    Criação das Assinaturas dos métodos da camada DAL
    O próximo passo vai ser implementar os métodos.

---
+++ GestaoComercial/DAL/ClienteDAL.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Models;
+
+namespace DAL
+{
+    public class ClienteDAL
+    {
+        public void Inserir(Cliente _cliente)
+        {
+
+        }
+        public void Alterar(Cliente _cliente)
+        {
+
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+        public List<Cliente> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GestaoComercial/DAL/ProdutoDAL.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Models;
+
+namespace DAL
+{
+    public class ProdutoDAL
+    {
+        public void Inserir(Produto _produto)
+        {
+
+        }
+        public void Alterar(Produto _produto)
+        {
+
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+        public List<Produto> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
