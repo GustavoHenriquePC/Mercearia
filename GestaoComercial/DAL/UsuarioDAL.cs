ba23ee1524bb671bd7a7014805d870cc8ab629c1
Gustavo Henrique <148218604+GustavoHenriquePC@users.noreply.github.com>
2023-10-20 11:26:13 -0300

    Implementação do método inserir na UsuarioDALL

--- GestaoComercial/DAL/UsuarioDAL.cs
+++ GestaoComercial/DAL/UsuarioDAL.cs
@@ -7,12 +7,24 @@ namespace DAL
 {
     public class UsuarioDAL
     {
-        public void Inserir(Usuario _ususario)
+        public void Inserir(Usuario _usuario)
         {
             try
             {
                 SqlConnection cn = new SqlConnection();
                 SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"INSERT INTO Usuario(Nome, NomeUsuario, Senha, Ativo)
+                 VALUES(@Nome, @NomeUsuario, @Senha,@Ativo)";
+
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Nome", _usuario.Nome);
+                cmd.Parameters.AddWithValue("@NomeUsuario", _usuario.NomeUsuario);
+                cmd.Parameters.AddWithValue("@Senha", _usuario.Senha);
+                cmd.Parameters.AddWithValue("@Ativo", _usuario.Ativo);
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
             }
             catch (Exception ex)
             {
